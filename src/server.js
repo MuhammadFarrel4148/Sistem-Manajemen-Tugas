cafe11724430bee2bbc37ca17fc3c52fa1aaad92
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-01-27 15:50:50 +0700

    Add file and server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async() => {
+    const server = Hapi.server({
+        port: 8080,
+        host: 'localhost',
+    })
+
+    await server.start();
+
+    console.log(`Server berjalan di ${server.info.uri}`);
+}
+
+init();
